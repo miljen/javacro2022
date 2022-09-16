3c040fcf934e328ce85857b2dca17725191e0a35
Miljen Stankovic <smiljen@gmail.com>
2022-09-16 23:50:56 +0200

    Health

--- src/main/java/com/miljen/demo/javacro2022/controller/DemoController.java
+++ src/main/java/com/miljen/demo/javacro2022/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.miljen.demo.javacro2022.controller;
 import com.miljen.demo.javacro2022.model.Predavanje;
 import com.miljen.demo.javacro2022.repository.PredavanjeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,11 @@ public class DemoController {
     public List<Predavanje> getPredavanja() {
 
         return (List<Predavanje>) repository.findAll();
+    }
+
+    @GetMapping("health")
+    public ResponseEntity<Void> getHealth() {
+
+        return ResponseEntity.ok().build();
     }
 }
